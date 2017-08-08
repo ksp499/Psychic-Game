ca11dc3f163ca34c5225478eaeac60a6d62cac31
ksp499 <ksp499@gmail.com>
2017-08-08 18:32:25 -0500

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,65 @@
-var container = document.getElementById("wrapper");
+// OBJECT CREATION
+// ==============================================================================
 
-container.innerHTML = "HaHa";
+var game = {
+	win : 0,
+	lose : 0,
+	guess_left : 9,
+	guess_done : [],
+	guessed : false,
+	letters : ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+	update_board : function() {
+		document.querySelector("#win").innerHTML = "Wins: " + game.win;
+		document.querySelector("#lose").innerHTML = "Losses: " + game.lose;
+		document.querySelector("#guess_left").innerHTML = "Guesses Left: " + game.guess_left;
+		document.querySelector("#guess_done").innerHTML = "Your Guesses so far: " + game.guess_done;
+	},
+	reset_board : function() {
+		game.guess_left = 9;
+		game.guess_done = [];
+		if (game.guessed) {
+			game.win++;
+		}
+		else {
+			game.lose++;
+		}
+		game.guessed = false;
+	},
+
+	choose_letter : function() {
+		return game.letters[Math.floor(Math.random() * game.letters.length)];
+	},
+	answer : ""
+};
+
+
+// MAIN PROCESS
+// ==============================================================================
+
+game.answer = game.choose_letter();
+console.log(game.answer);
+
+document.onkeyup = function(event) {
+
+	var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+
+	//when user gets it right
+	if (userInput === game.answer) {
+		game.guessed = true;
+		game.reset_board();
+		game.update_board();
+	}
+	//when user misses the answer
+	else {
+		game.guess_left--;
+		game.guess_done.push(userInput);
+		if (game.guess_left == 0) {
+			game.reset_board();
+		}
+		game.update_board();
+	}
+
+};
+
+
+
